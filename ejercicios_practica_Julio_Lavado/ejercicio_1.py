568035b24a81ed6be51cbfe073c3319fe690682a
jucelaya <115673563+jucelaya@users.noreply.github.com>
2022-10-25 18:15:50 -0500

    Add files via upload

---
+++ ejercicios_practica_Julio_Lavado/ejercicio_1.py
@@ -0,0 +1,33 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 3.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica
+
+numero_1 = 5
+numero_2 = 7
+
+# Realizar la suma de las dos variables
+
+# Crear una variable llamada suma donde se 
+# almacene el valor de la suma de las variable numero_1 y numero_2
+# ej:
+# suma = .....
+suma=numero_1 + numero_2
+
+# Imprimir en pantalla el resultado de la suma
+# print(....)
+print('La suma es: ',suma)
+# Repita el procedimiento para realizar la resta
+resta=numero_1 - numero_2
+# Crear una variable llamada resta donde se 
+print('La resta es: ',resta)
+# almacene el valor de la resta de numero_1 menos numero_2
+
+
+# Imprimir en pantalla el resultado de la resta
---
+++ ejercicios_practica_Julio_Lavado/ejercicio_2.py
@@ -0,0 +1,42 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 3.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios de práctica numérica y consola
+
+# Ahora los valores a operar deben ser ingresados por
+# consola con la función "input" como se ve a continuación
+numero_1 = int(input('Ingrese por consola el primer número entero a operar:'))
+
+numero_2 = int(input('Ingrese por consola el segundo número entero a operar:'))
+
+# Alumno: Imprima en pantalla los dos números enteros solicitados
+# print(....)
+print('Los numeros ingresados son:',numero_1,'y',numero_2)
+# Alumno: Calcule la suma, resta, división y multiplicación
+# de los números ingresados numero_1, numero_2
+
+# Crear una variable llamada suma donde se 
+# almacene el valor de la suma de las variable numero_1 y numero_2
+a = numero_1 + numero_2
+
+# Crear una variable llamada resta donde se 
+# almacene el valor de la resta de numero_1 menos numero_2
+b = numero_1 - numero_2
+# Crear una variable llamada division donde se 
+# almacene el valor de la division de numero_1 dividido numero_2
+c = numero_1 / numero_2
+# Crear una variable llamada multiplicacion donde se 
+# almacene el valor de la multiplicacion de numero_1 por numero_2
+d = numero_1 * numero_2
+
+# Imprima en pantalla todos los resultados
+print('La suma es:',a)
+print('La resta es:',b)
+print('La division es:',c)
+print('La multiplicación es:',d)
